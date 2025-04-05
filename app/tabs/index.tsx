d6b5bfc2364963634a15d4ef074f3e8210f61682
alizenart <97767073+alizenart@users.noreply.github.com>
2025-04-05 14:51:08 -0500

    image picker done

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,24 +1,50 @@
-import { View, StyleSheet } from 'react-native';
-import { Image } from 'expo-image'; 
+import { View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import { useState } from "react";
 
-import Button from '@/components/Button';
-import ImageViewer from '@/components/ImageViewer';
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
 
+import * as ImagePicker from "expo-image-picker";
 
-const PlaceholderImage = require('@/assets/images/react-logo.png');
-
+const PlaceholderImage = require("@/assets/images/react-logo.png");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  const pickImageAsync = async () => {
+    console.log("pickImageAsync");
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
-
     </View>
   );
 }
@@ -26,8 +52,8 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
   },
   imageContainer: {
     flex: 1,
@@ -39,6 +65,6 @@ const styles = StyleSheet.create({
   },
   footerContainer: {
     flex: 1 / 3,
-    alignItems: 'center',
-  }
+    alignItems: "center",
+  },
 });
